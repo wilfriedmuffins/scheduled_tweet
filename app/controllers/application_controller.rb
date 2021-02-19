990531f64acce529c61ad6d7de6eabea7973bd18
wilfriedmuffins <wilfried.moffen@live.fr>
2021-02-19 17:13:38 +0100

    navbar accessing user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+#avant chauqe action set_current_usersera exercuté
+before_action :set_current_user
+
+
+    #action see for all controller
+    def set_current_user
+        if session[:user_id]
+            Current.user= User.find_by(id: session[:user_id])
+        end
+    end
+
 end
