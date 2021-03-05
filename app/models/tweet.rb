9420896ddbcc3a95ada28d825aac68218b420859
wilfriedmuffins <wilfried.moffen@live.fr>
2021-03-05 15:38:14 +0100

    pb resolved: no display errors

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -2,7 +2,7 @@ class Tweet < ApplicationRecord
   belongs_to :user
   belongs_to :twitter_account
 
-  validates :body, length: { minimun: 1, maximum: 280 }
+  validates :body, length: { minimum: 1, maximum: 280 }
   validates :publish_at, presence: true
 
   after_initialize do
